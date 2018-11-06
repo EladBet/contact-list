053a2e9b332a49308e2343a63ba720c9973df501
EldadB <Eladb@playbuzz.vom>
2018-11-06 09:58:37 +0200

    Observable

--- src/index.js
+++ src/index.js
@@ -5,45 +5,27 @@ import Nav from './navbar/navbar.component'
 import './style.scss';
 import { getContacts } from './contacts.service.js';
 
-const maxItemsToRender = 12;
-
 class App extends Component {
   constructor() {
     super();
-    this.state = {     
+    this.state = {
       contacts: [],
       loaded: false,
       error: '',
-      showMore: false
     };
-    this.allContacts = [];
-    this.count = 0;
-
-    this.showMore = this.showMore.bind(this);
   }
 
   componentDidMount() {
     getContacts()
       .then(response => response.json())
       .then(contacts => {
-        this.allContacts = contacts;
         this.count++;
         this.setState({
           loaded: true,
-          contacts: this.allContacts.slice(0, maxItemsToRender),
-          showMore: contacts.length > maxItemsToRender
+          contacts: contacts
         });
       })
       .catch(error => { this.setState({ loaded: true, error }); })
-  }
-
-  showMore() {
-    this.count++;
-    if (maxItemsToRender * this.count >= this.allContacts.length) {
-      this.setState({ contacts: this.allContacts.slice(0), showMore: false });
-    } else {
-      this.setState({ contacts: this.allContacts.slice(0, maxItemsToRender * this.count) });
-    }
   }
 
   render() {
@@ -56,21 +38,11 @@ class App extends Component {
           {this.state.loaded && !this.state.error && (
             <div className="contacts-container">
               {this.state.contacts.length > 0 ? this.state.contacts.map((contact, index) =>
-                <Contact index={index} {...contact} />
+                <Contact key={index} {...contact} />
               ) : 'No Available Contacts Found :-('}
             </div>
           )}
         </div>
-        {this.state.showMore && (
-          <div className="more-btn">
-            <button
-              type="button"
-              onClick={this.showMore}
-              className="btn btn-secondary btn-lg"
-            > Show More
-            </button>
-          </div>
-        )}
       </div>
     );
   }
