f5b91dfece60a7f7186067d6f46226f8d8a18672
EldadB <Eladb@playbuzz.vom>
2018-11-06 10:29:38 +0200

    Updates

---
+++ precache-manifest.37b89162b708ac3ba6bd2f27bfea229c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "377b31ea3711a9d6ba50",
+    "url": "/contact-list/static/js/runtime~main.377b31ea.js"
+  },
+  {
+    "revision": "9085d7c857f50ad6da5e",
+    "url": "/contact-list/static/js/main.9085d7c8.chunk.js"
+  },
+  {
+    "revision": "b7755aa9c8d3fdd90379",
+    "url": "/contact-list/static/js/1.b7755aa9.chunk.js"
+  },
+  {
+    "revision": "9085d7c857f50ad6da5e",
+    "url": "/contact-list/static/css/main.f9ef6ee1.chunk.css"
+  },
+  {
+    "revision": "b7755aa9c8d3fdd90379",
+    "url": "/contact-list/static/css/1.5d936a2b.chunk.css"
+  },
+  {
+    "revision": "684129e093fdad223972c41e3f0236f6",
+    "url": "/contact-list/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/honybook-contact-list/precache-manifest.aa41b57a18c02a6ad65364dd23cd3793.js"
+  "/contact-list/precache-manifest.37b89162b708ac3ba6bd2f27bfea229c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/honybook-contact-list/index.html", {
+workbox.routing.registerNavigationRoute("/contact-list/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
